2e76b1161bb9ca6c7ffec57eacef07459bd46096
tnmotorman <steve_cross@yahoo.com>
2021-03-22 13:24:43 -0500

    Lesson 94

--- App.js
+++ App.js
@@ -1,13 +1,41 @@
-import { StatusBar } from "expo-status-bar";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+  StatusBar,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Blank Canvas</Text>
-    </View>
+    <>
+      <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
+        <View
+          style={{
+            padding: 16,
+            backgroundColor: "green",
+          }}
+        >
+          <Text>Search</Text>
+        </View>
+        <View
+          style={{
+            flex: 1,
+            padding: 16,
+            backgroundColor: "blue",
+          }}
+        >
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  searchContainer: {},
+});
